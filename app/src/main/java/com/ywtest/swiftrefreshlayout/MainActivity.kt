afa1f09c1c6c2118a2f5d5cf24897e6c30bf1693
ywlee861009 <ywlee861009@gmail.com>
2019-01-29 01:32:33 +0900

    Databinding gradle enabled

--- app/src/main/java/com/ywtest/swiftrefreshlayout/MainActivity.kt
+++ app/src/main/java/com/ywtest/swiftrefreshlayout/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ywtest.swiftrefreshlayout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
