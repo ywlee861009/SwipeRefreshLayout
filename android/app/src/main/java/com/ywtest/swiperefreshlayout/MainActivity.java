7ac523008ae83cdd9d3f1fd9195607b11102b96b
ywlee861009 <ywlee861009@gmail.com>
2019-01-29 02:41:33 +0900

    supportv4 추가

--- android/app/src/main/java/com/ywtest/swiperefreshlayout/MainActivity.java
+++ android/app/src/main/java/com/ywtest/swiperefreshlayout/MainActivity.java
@@ -8,6 +8,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initView();
+    }
+
+
+    /**
+     * 뷰 초기화
+     */
+    private void initView() {
         setContentView(R.layout.activity_main);
     }
 }
